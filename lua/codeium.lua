c67dcbc2bf6d42cd3d45dcc7562242faba3512ee
ratnaraj7 <ratnarajdaimary7@gmail.com>
2024-04-02 06:51:20 +0530

    add codeium

--- lua/codeium.lua
+++ lua/codeium.lua
@@ -1,3 +1,20 @@
 return {
-	"Exafunction/codeium.vim"
+	"Exafunction/codeium.vim",
+	config = function()
+		--vim.g.codeium_manual = true
+		vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end,
+			{ expr = true, silent = true })
+
+		vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+			{ expr = true, silent = true })
+
+		vim.keymap.set('i', '<C-/>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+			{ expr = true, silent = true })
+
+		vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end,
+			{ expr = true, silent = true })
+
+		--vim.keymap.set('i', '<C-b>', function() return vim.fn['codeium#Chat']() end,
+		--{ expr = true, silent = true })
+	end
 }
--- lua/lua-line.lua
+++ lua/lua-line.lua
@@ -150,6 +150,13 @@ return {
 			},
 		})
 
+		ins_left({
+			function()
+				vim.api.nvim_call_function("codeium#GetStatusString", {})
+			end
+		})
+
+
 		-- Insert mid section. You can make any number of sections in neovim :)
 		-- for lualine it's any number greater then 2
 		ins_left({
