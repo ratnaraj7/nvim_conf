efd95c5809ff68d69d3997fb74df031bb167c69a
ratnaraj7 <ratnarajdaimary7@gmail.com>
2024-05-28 18:48:35 +0530

    add clipboard option wayland

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,15 @@
 vim.g.mapleader = " "
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
+
+let g:clipboard = {
+    \   'copy': {
+    \       '+': ['wl-copy', '--trim-newline'],
+    \       '*': ['wl-copy', '--trim-newline'],
+    \   },
+    \   'paste': {
+    \       '+': ['wl-paste', '--no-newline'],
+    \       '*': ['wl-paste', '--no-newline'],
+    \   },
+    \ }
+
