3870b873eb37a12148503268d5d087bb5c487754
ratnaraj7 <ratnarajdaimary7@gmail.com>
2024-05-28 19:27:15 +0530

    fix wayland copy/pasta

--- lua/options.lua
+++ lua/options.lua
@@ -2,14 +2,3 @@ vim.g.mapleader = " "
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
 
-let g:clipboard = {
-    \   'copy': {
-    \       '+': ['wl-copy', '--trim-newline'],
-    \       '*': ['wl-copy', '--trim-newline'],
-    \   },
-    \   'paste': {
-    \       '+': ['wl-paste', '--no-newline'],
-    \       '*': ['wl-paste', '--no-newline'],
-    \   },
-    \ }
-
